e8bc2df395fedb02845b68e7ad5730be182821c8
Ulyana Kryshchuk <ulyana.kryshchuk@gmail.com>
2026-04-07 14:34:10 +0300

    added implementation od Queue using Stack. Added tests_passed screenshot

--- Implement_Queue_using_Stacks/main.py
+++ Implement_Queue_using_Stacks/main.py
@@ -1,18 +1,89 @@
 """ my queue """
-from stack import Stack
+
+
+class Stack:
+    """A stack implementation."""
+
+    def __init__(self):
+        """Initialize an empty stack."""
+        self._items = []
+
+    def is_empty(self):
+        """
+        Check whether the stack is empty.
+
+        :return: True if empty, False otherwise.
+        """
+        return len(self._items) == 0
+
+    def push(self, item):
+        """
+        Add an element to the top of the stack.
+
+        :param item: Element to add.
+        """
+        self._items.append(item)
+
+    def pop(self):
+        """
+        Remove and return the top element.
+
+        :raises IndexError: If the stack is empty.
+        :return: Removed element.
+        """
+        if self.is_empty():
+            raise IndexError("The stack is empty. Can't pop.")
+        return self._items.pop()
+
+    def peek(self):
+        """
+        Return the top element without removing it.
+
+        :raises IndexError: If the stack is empty.
+        :return: Top element.
+        """
+        if self.is_empty():
+            raise IndexError("The stack is empty. Can't peek.")
+        return self._items[-1]
+
+    def __len__(self):
+        """Return the number of elements in the stack."""
+        return len(self._items)
+
+    def __str__(self):
+        """Return string representation of the stack."""
+        return "Stack: " + " -> ".join(map(str, reversed(self._items)))
 
 
 class MyQueue:
+    """ myqueue """
 
     def __init__(self):
+        """ init """
+        self.stack_in = Stack()
+        self.stack_out = Stack()
 
     def push(self, x: int) -> None:
+        """ push """
+        self.stack_in.push(x)
 
     def pop(self) -> int:
+        """ pop """
+        if self.stack_out.is_empty():
+            while not self.stack_in.is_empty():
+                self.stack_out.push(self.stack_in.pop())
+        return self.stack_out.pop()
 
     def peek(self) -> int:
+        """ peek """
+        if self.stack_out.is_empty():
+            while not self.stack_in.is_empty():
+                self.stack_out.push(self.stack_in.pop())
+        return self.stack_out.peek()
 
     def empty(self) -> bool:
+        """ empty """
+        return self.stack_out.is_empty() and self.stack_in.is_empty()
 
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
