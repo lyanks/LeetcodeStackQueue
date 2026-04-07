2dc4d248f563e669d5ff7943e8ea4bc717941ee4
Ulyana Kryshchuk <ulyana.kryshchuk@gmail.com>
2026-04-07 22:52:27 +0300

    added implementation of FreqStack with dictionaries. added tests passed

--- Maximum_Frequency_Stack/main.py
+++ Maximum_Frequency_Stack/main.py
@@ -1,76 +1,78 @@
-from collections import deque
+"""myfreqstack"""
+
+
+class Stack:
+    """A stack implementation."""
+
+    def __init__(self):
+        """Initialize an empty stack."""
+        self._items = []
+
+    def is_empty(self):
+        """
+        Check whether the stack is empty.
+
+        :return: True if empty, False otherwise.
+        """
+        return len(self._items) == 0
+
+    def push(self, item):
+        """
+        Add an element to the top of the stack.
+
+        :param item: Element to add.
+        """
+        self._items.append(item)
+
+    def pop(self):
+        """
+        Remove and return the top element.
+
+        :raises IndexError: If the stack is empty.
+        :return: Removed element.
+        """
+        if self.is_empty():
+            raise IndexError("The stack is empty. Can't pop.")
+        return self._items.pop()
+
+    def peek(self):
+        """
+        Return the top element without removing it.
+
+        :raises IndexError: If the stack is empty.
+        :return: Top element.
+        """
+        if self.is_empty():
+            raise IndexError("The stack is empty. Can't peek.")
+        return self._items[-1]
+
+    def __len__(self):
+        """Return the number of elements in the stack."""
+        return len(self._items)
+
+    def __str__(self):
+        """Return string representation of the stack."""
+        return "Stack: " + " -> ".join(map(str, reversed(self._items)))
 
 
 class FreqStack:
-    """freqstack"""
-
     def __init__(self):
-        self.data = deque()
+        self.freq = {}
+        self.group = {}
+        self.max_freq = 0
 
     def push(self, val: int) -> None:
-        """ push """
-        self.data.append(val)
+        f = self.freq.get(val, 0) + 1
+        self.freq[val] = f
+        if f > self.max_freq:
+            self.max_freq = f
+        if f not in self.group:
+            self.group[f] = Stack()
+        self.group[f].push(val)
 
     def pop(self) -> int:
-        """ pop """
-        freq = deque()
-
-        def add_count(v: int) -> None:
-            tmp = deque()
-            found = False
-            while freq:
-                pair = freq.pop()
-                if pair[0] == v:
-                    pair[1] += 1
-                    found = True
-                tmp.append(pair)
-            while tmp:
-                freq.append(tmp.pop())
-            if not found:
-                freq.append([v, 1])
-
-        def get_count(v: int) -> int:
-            count = 0
-            tmp: deque = deque()
-            while freq:
-                pair = freq.pop()
-                tmp.append(pair)
-                if pair[0] == v:
-                    count = pair[1]
-            while tmp:
-                freq.append(tmp.pop())
-            return count
-
-        tmp: deque = deque()
-        while self.data:
-            v = self.data.pop()
-            tmp.append(v)
-            add_count(v)
-        while tmp:
-            self.data.append(tmp.pop())
-        max_freq = 0
-        tmp2: deque = deque()
-        while freq:
-            pair = freq.pop()
-            tmp2.append(pair)
-            if pair[1] > max_freq:
-                max_freq = pair[1]
-        while tmp2:
-            freq.append(tmp2.pop())
-
-        result = None
-        found = False
-        scratch: deque = deque()
-
-        while self.data:
-            v = self.data.pop()
-            if not found and get_count(v) == max_freq:
-                result = v
-                found = True
-            else:
-                scratch.appendleft(v)
-
-        while scratch:
-            self.data.append(scratch.popleft())
-
-        return result
+        val = self.group[self.max_freq].pop()
+        self.freq[val] -= 1
+        if self.group[self.max_freq].is_empty():
+            self.max_freq -= 1
+        return val
